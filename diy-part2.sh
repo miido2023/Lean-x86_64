4d1cdde0eadc7c99c64f01034fff3961e4536cf2
chenshuojian <55643426+hubumulity@users.noreply.github.com>
2021-12-21 09:15:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,9 +6,18 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# File name: diy-part02.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+#软件合集
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small-package package/openwrt-samll
+
+#添加额外软件包
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
